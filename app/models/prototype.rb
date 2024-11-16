fd90e6247c684d10fd036f9886e48fc17dfa3415
Matsu-sh821 <m.shouta128@gmail.com>
2024-11-16 09:51:22 +0900

    second commit

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -3,7 +3,7 @@ class Prototype < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-  validates :image, presence: true
+  validates :title, presence: true
   validates :catch_copy, presence: true
   validates :concept, presence: true
   validates :image, presence: true
